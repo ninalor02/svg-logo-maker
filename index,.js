80052d6fad48888fcd42ac3497525925be3b35ba
nina <128232339+ninalor02@users.noreply.github.com>
2023-07-12 21:11:26 -0500

    created, adding notes to shape.test.js

--- index,.js
+++ index,.js
@@ -1,3 +1,13 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+const fileSystem = require('./node_modules/graceful-fs/graceful-fs');
 const {Triangle,Square, Circle} = require("./lib/shapes");
+
+class Svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement =''
+    }
+    render(){
+        
+    }
+}
