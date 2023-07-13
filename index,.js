26a66f892878834a3955cbbc63c0858f0d07d34b
nina <128232339+ninalor02@users.noreply.github.com>
2023-07-12 21:57:59 -0500

    adding questions inside index.js

--- index,.js
+++ index,.js
@@ -2,6 +2,31 @@ const inquirer = require("inquirer");
 const fileSystem = require('./node_modules/graceful-fs/graceful-fs');
 const {Triangle,Square, Circle} = require("./lib/shapes");
 
+const questions = [
+    { // choose a shape
+    type: 'list',
+    name: 'shape',
+    message: 'Please choose which shape?',
+    choices: ['circle', 'triangle', 'square'],
+},
+{   // color
+    type: 'input',
+    name: 'shape-color',
+    message: 'Please enter in a desire color?',
+},
+{   //text input for what the logo will be
+    type: 'list',
+    name: 'text',
+    message: 'Please enter in 3(Three) letters in CAPS?',
+},
+{
+    type: 'list',
+    name: 'text-color',
+    message: 'Please enter a color that you would like the text to be?',
+    choices: ['circle', 'triangle', 'square'],
+},
+]
+
 class Svg{
     constructor(){
         this.textElement = ''
